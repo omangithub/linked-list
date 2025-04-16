65e1e65635b490f8f0608daafd99a997c07147de
oman <smithowain@hotmail.com>
2025-04-16 14:20:56 +0900

    first commit of linked list project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import "./styles.css";
 
+console.log("Hello world")
+
+class node {
+    value=null;
+    nextNode=null;
+
+    constructor (value, nextNode) {
+        this.value=value,
+        this.nextNode=nextNode
+    }
+}
+
+class LinkedList {
+    
+constructor () {
+    this.head=null;
+    this.length=0;
+}
+
+    append(data) {
+       const node = new node (data)
+    },
+
+
+}
